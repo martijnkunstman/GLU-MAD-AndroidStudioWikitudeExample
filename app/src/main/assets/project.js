c86ff9983cfbf54d2b323585651f0d64e14202c7
martijnkunstman <martijn@kunstman.nl>
2018-09-27 23:59:28 +0200

    Wikitude Studio Project Import

---
+++ app/src/main/assets/project.js
@@ -0,0 +1,104 @@
+// JSON file describing the AR-experience for offline usage
+var projectJSONOffline =
+{
+    "isCloud": false,
+    "tcId": "5bad3e3e3ac80f5028edc4c5",
+    "name": "GLU LOGO",
+    "targets": [
+        {
+            "name": "glu",
+            "augmentations": [
+                {
+                    "type": "VideoDrawable",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/102448_glu/5bad504bf2f66950186cf0fa/video.mp4",
+                    "targetId": "5bad3e4828f1315023aec6b2",
+                    "externalUri": false,
+                    "firstFrameImgUrlFileSize": 32695,
+                    "firstFrameAlphaImgUrlFileSize": 20841,
+                    "firstFrameAlphaImgUrl": "augmentation-assets/102448_glu/5bad504bf2f66950186cf0fa/firstFrameAlphaImgUrl.png",
+                    "minSDK": "6.1.0",
+                    "modDat": 1538085178074,
+                    "creDat": 1538084939148,
+                    "properties": {
+                        "name": "heart",
+                        "firstFrameImgUrl": "augmentation-assets/102448_glu/5bad504bf2f66950186cf0fa/firstFrameImg.png",
+                        "translate": {
+                            "x": -0.003,
+                            "y": -0.003,
+                            "z": 0.25
+                        },
+                        "autoPlay": true,
+                        "autoResume": true,
+                        "endlessLoop": true,
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 8,
+                            "y": 8,
+                            "z": 8
+                        },
+                        "videoMode": "overlayAlpha",
+                        "zOrder": 0
+                    },
+                    "fileSize": 717927,
+                    "id": "5bad504bf2f66950186cf0fa"
+                }
+            ]
+        }
+    ],
+    "type": "ImageTargetCollection",
+    "wtcFiles": [
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.0",
+            "creDat": 1538080346637
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.1",
+            "creDat": 1538080346637
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.2",
+            "creDat": 1538080346637
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.0",
+            "creDat": 1538080346637
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.1",
+            "creDat": 1538080346637
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.2",
+            "creDat": 1538080346637
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "8.0",
+            "creDat": 1538080346637
+        }
+    ],
+    "settings": {
+        "tracker": {}
+    }
+}
+;
---
+++ app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/WikitudeSDKConstants.java
@@ -0,0 +1,11 @@
+package com.wikitude.wikitudestudioandroidapptemplate;
+
+public class WikitudeSDKConstants {
+	
+
+	/**
+	 * enter valid license key here to get rid of watermarkin. Note: Ensure to choose a new and unique package name up front (e.g. com.yourcompany.yourappname)
+	 */
+	protected static final String WIKITUDE_SDK_KEY = "";
+
+}
