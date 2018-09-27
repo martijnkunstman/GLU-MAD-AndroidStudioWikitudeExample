b604b1a8798d331cb4b1d6e283f03d038d6ea8e4
martijnkunstman <martijn@kunstman.nl>
2018-09-28 00:11:07 +0200

    Add key.

--- app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/WikitudeSDKConstants.java
+++ app/src/main/java/com/wikitude/wikitudestudioandroidapptemplate/WikitudeSDKConstants.java
@@ -6,6 +6,6 @@ public class WikitudeSDKConstants {
 	/**
 	 * enter valid license key here to get rid of watermarkin. Note: Ensure to choose a new and unique package name up front (e.g. com.yourcompany.yourappname)
 	 */
-	protected static final String WIKITUDE_SDK_KEY = "";
+	protected static final String WIKITUDE_SDK_KEY = "STuyVorajNqfcPhgaM7ke8Uz2digAxHaenOt1VCF1tm5aIqD5y3L1DnMiMhQ8ZAwSR56QBKAXVYNpB+JlNov2Feo0W8ekfU5qKHMNd+0Xofr0rn9YrdfLDWhJarhP43wm1fQN1sxD3km3cdE7KWMbjM7tEv3SJPJ+gKHNClinqhTYWx0ZWRfXylczpURnBnGlS+fqpuG9S2UAgG+BQcfuZsWzFdwIY9BVxlDm/V68rubC/5n8vUzdzEbz1V3yxiVNNyZfHMEurmrN91gak/5b2ORnbvdAt3WH1HpVEUH6mK8Yi9q+rBbiFB039IOdjZpEB5xh2ytXaL78p8yJPmhzi/PJ2bEX+ywD4mVFvkPiuoN6bAV0gYEA5McxKOEb7FfOHntV7hGI4YS7MIgUlC/PCIiqJ4B5j+aIfuQAjCB1dDd6NBDP9pd9v0uHqD6+5movhURsOi48jn+GrCMGfpjZ99RLlX8tN9bs7n8ypTe/o6pygs76MRdW34CZnxGCILRIl1whqJlV1M25C/Uu1349us/KjqxYlsTxRFbzNQ4ATiPCcjLhlf3/nNKLKLZPXLKLpI/F6fdY3GmXhlMsV8H0QtESVW9yWXXLd7e8ESirRKLB4q11tY7yMmgVsUKlKjGy8MxTv4fRuZx6xOX0oW0vYNyH1ObE/BhHi3zCYeBqrHOgXRFfN1a+NQvcNwLU8B9ZO55A0zo1wZ5Q4lQq7iBlQ==";
 
 }
